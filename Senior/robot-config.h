56ecaf7d0caf1545f67f38b727e29bf323e514c3
Robotics-Coding <66975669+Robotics-Coding@users.noreply.github.com>
2021-11-19 15:09:32 -0500

    Create robot-config.h

---
+++ Senior/robot-config.h
@@ -0,0 +1,31 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor_group Drive_Left;
+extern motor_group Drive_Right;
+extern motor_group rollers;
+extern motor LeftBack;
+extern motor RightBack;
+extern motor LeftFront;
+extern motor RightFront;
+extern motor Intake;
+extern motor Intake2;
+extern motor Lift1;
+extern motor roller;
+extern motor highRoller;
+extern motor index_roller;
+extern motor front_lift;
+extern controller Controller1;
+
+extern inertial TurnGyroSmart;
+
+extern smartdrive Drivetrain;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
